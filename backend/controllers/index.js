5314e82d377257099d2e406d971c5b566cf05c5b
devin <devin1891@gmail.com>
2016-07-20 01:16:33 -0700

    adding the apis

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const helpers = require('../helpers')
 
 let router = express.Router();
 
 router.use('/movie', require('./movie'))
 
 router.get('/', function (req, res) {
-  res.status(200).send('OK')
+  helpers.getMovieConfiguration(function(movieConfig){
+    res.status(200).send(movieConfig);
+  });
 });
 
 
--- backend/controllers/movie.js
+++ backend/controllers/movie.js
@@ -1,6 +1,53 @@
 const express = require('express');
+const request = require('request');
+
+
+const config = require('../config');
 
 let router = express.Router();
+
+
+router.get('/popular/:page?',function (req, res) {
+  request({
+    method: 'GET',
+    url: config.api_root + '/movie/popular',
+    qs: {
+      api_key: config.api_key,
+      page: req.params.page || 1
+    }, headers: {
+      'Accept': 'application/json'
+    }}, function (error, response, body) {
+      res.status(200).send(body);
+  });
+});
+
+router.get('/search/:page?',function (req, res) {
+  request({
+    method: 'GET',
+    url: config.api_root + '/search/movie',
+    qs: {
+      api_key: config.api_key,
+      query: req.query.query || '',
+      page: req.params.page || 1
+    }, headers: {
+      'Accept': 'application/json'
+    }}, function (error, response, body) {
+      res.status(200).send(body);
+  });
+});
+
+router.get('/info/:id',function (req, res) {
+  request({
+    method: 'GET',
+    url: config.api_root + '/movie/' + req.params.id,
+    qs: {
+      api_key: config.api_key,
+    }, headers: {
+      'Accept': 'application/json'
+    }}, function (error, response, body) {
+      res.status(200).send(body);
+  });
+});
 
 router.get('/', function (req, res) {
   // TODO actually implement this
